863bff8544d60f63655894421cb4cbc51acb54a2
Paulo Messias <paulohenrique1403@yahoo.com>
2022-01-31 12:10:22 -0300

    update coverage and add new controller

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,36 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const hours = [11, 12];
+    const [appointment1, appointment2] = await Promise.all(
+      hours.map(async hour =>
+        fakeAppointmentsRepository.create({
+          provider_id: 'provider-id',
+          user_id: 'user-id',
+          date: new Date(2022, 1, 2, hour, 0, 0),
+        }),
+      ),
+    );
+
+    const appointments = await listProviderAppointmentsService.execute({
+      provider_id: 'provider-id',
+      day: 2,
+      year: 2022,
+      month: 2,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
